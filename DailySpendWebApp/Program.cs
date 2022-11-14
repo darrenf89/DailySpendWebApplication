e13b1602acadfb83f619ee82f05b6d28ffd32f3c
darrenf89 <88579182+darrenf89@users.noreply.github.com>
2022-11-14 22:25:51 +0000

    14/11/22 Commit - EnterBudgetDetails Drop Down Menus and JS

--- DailySpendWebApp/Program.cs
+++ DailySpendWebApp/Program.cs
@@ -8,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 builder.Services.AddDbContext<ApplicationDBContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnectionString")
     ));
