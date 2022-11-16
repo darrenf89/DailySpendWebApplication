d3458fb839b7c5b08dd9d687b865887cfbb297f0
darrenf89 <darren.fillis100@gmail.com>
2022-11-16 18:16:08 +0000

    (laptop) Commit

--- DailySpendWebApp/Models/CurrencySelector.cs
+++ DailySpendWebApp/Models/CurrencySelector.cs
@@ -2,13 +2,13 @@
 {
     public class CurrencySelector
     {
-        public string symbol { get; set; }
-        public string name { get; set; }
-        public string symbol_native { get; set; }
-        public int decimal_digits { get; set; }
-        public decimal rounding { get; set; }
-        public string code { get; set; }
-        public string name_plural { get; set; }
+        public string? symbol { get; set; } = default!;
+        public string? name { get; set; }
+        public string? symbol_native { get; set; }
+        public int? decimal_digits { get; set; }
+        public decimal? rounding { get; set; }
+        public string? code { get; set; }
+        public string? name_plural { get; set; }
 
     }
 }
