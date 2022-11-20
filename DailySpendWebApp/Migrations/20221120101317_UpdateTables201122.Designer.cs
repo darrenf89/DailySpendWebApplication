747dc473e13654d6c94c85ee08bb6c1cbfb89498
darrenf89 <88579182+darrenf89@users.noreply.github.com>
2022-11-20 20:17:06 +0000

    201122 Home

--- DailySpendWebApp/Migrations/20221120101317_UpdateTables201122.Designer.cs
+++ DailySpendWebApp/Migrations/20221120101317_UpdateTables201122.Designer.cs
@@ -4,6 +4,7 @@ using DailySpendBudgetWebApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DailySpendBudgetWebApp.Migrations
 {
     [DbContext(typeof(ApplicationDBContext))]
-    partial class ApplicationDBContextModelSnapshot : ModelSnapshot
+    [Migration("20221120101317_UpdateTables201122")]
+    partial class UpdateTables201122
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
