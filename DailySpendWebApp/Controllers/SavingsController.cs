32655fe1228274d5571275b16d79e72c7d63184e
darrenf89 <88579182+darrenf89@users.noreply.github.com>
2022-11-26 22:25:49 +0000

    26112922

---
+++ DailySpendWebApp/Controllers/SavingsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DailySpendWebApp.Controllers
+{
+    public class SavingsController : Controller
+    {
+        public IActionResult AddSaving()
+        {
+            TempData["NickName"] = HttpContext.Session.GetString("_Name");
+            TempData["PageHeading"] = "Add A New Saving!";
+            return View();
+        }
+    }
+}
