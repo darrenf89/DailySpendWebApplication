7227afc2600fe085c5418a22553b453c7aea87e5
darrenf89 <88579182+darrenf89@users.noreply.github.com>
2022-12-08 18:21:54 +0000

    Commit

---
+++ DailySpendWebApp/Migrations/20221208175804_mysql.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DailySpendBudgetWebApp.Migrations
+{
+    public partial class mysql : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "SavingsName",
+                table: "Savings",
+                type: "nvarchar(15)",
+                maxLength: 15,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "SavingsGoal",
+                table: "Savings",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "GoalDate",
+                table: "Savings",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GoalDate",
+                table: "Savings");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SavingsName",
+                table: "Savings",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(15)",
+                oldMaxLength: 15);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "SavingsGoal",
+                table: "Savings",
+                type: "decimal(18,2)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+        }
+    }
+}
