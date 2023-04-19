5a2a20e3053f57e77463f2a4bcba765af9b37f85
darrenf89 <darren.fillis100@gmail.com>
2023-04-19 17:05:12 +0100

    230419 train commit

--- DailySpendWebApp/Controllers/TransactionController.cs
+++ DailySpendWebApp/Controllers/TransactionController.cs
@@ -1,13 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿
+using Microsoft.AspNetCore.Mvc;
+using DailySpendBudgetWebApp.Models;
+using Microsoft.AspNetCore.Authorization;
+using DailySpendBudgetWebApp.Data;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Text.Json;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using DailySpendBudgetWebApp.Migrations;
+using System.Numerics;
+using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.Http;
+using System.Globalization;
+using Microsoft.Data.SqlClient.Server;
+using static System.Net.Mime.MediaTypeNames;
+using DailySpendWebApp.Services;
+using System.Linq;
 
 namespace DailySpendWebApp.Controllers
 {
     public class TransactionController : Controller
     {
-        public IActionResult AddTransaction()
+        public IActionResult AddTransaction(Transactions obj)
         {
             TempData["PageHeading"] = "Add a Transaction!";
-            return View();
+            return View(obj);
         }
     }
 }
